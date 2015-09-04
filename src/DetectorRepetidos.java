14bbdc95c18ecaf1ac53994fe7a1b25374a7f5cc
FlorEtcheverry <florencia.a.etcheverry@gmail.com>
2015-09-04 17:27:16 +0000

    Esto no se hace. Enunciado + cosa que no compila.

---
+++ src/DetectorRepetidos.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+public class DetectorRepetidos implements Runnable {
+
+	private LinkedBlockingQueue<String> colaURLsEntrantes;
+	private LinkedBlockingQueue<String> colaURLsNuevos;
+	
+	public DetectorRepetidos(LinkedBlockingQueue<String> colaEntrante,LinkedBlockingQueue<String> colaSaliente){
+		colaURLsEntrantes = colaEntrante;
+		colaURLsNuevos = colaSaliente;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		//lee de la cola un url
+		
+		//se fija en el archivo si ese url ya fue analizado
+		//si no lo fue, lo agrega al archivo y lo pone en la otra cola de urls no repetidos
+
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//por parámetro viene la primer URL
+		
+		//lee de arch de config cantidad de threads por pool
+		int cantDetectores;
+		
+		//crea colas (5)
+		
+		//crea pools con cant leidas		
+		//pool para detector repetidos
+		ExecutorService poolDetectores = Executors.newFixedThreadPool(cantDetectores);
+		
+		
+		//pool para html getter
+		
+		//pool html parser
+		
+		//pool resource downloader (uno por tipo?)
+		
+		//monitor (pool?)
+
+	}
+
+}
