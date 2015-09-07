ba70909636baaa941d4429f21481ffd722e44e6a
FlorEtcheverry <florencia.a.etcheverry@gmail.com>
2015-09-07 11:11:00 +0000

    ver TODOs en parser por errores

--- src/HtmlParser.java
+++ src/HtmlParser.java
@@ -26,10 +26,13 @@ public class HtmlParser implements Runnable {
 			try {
 				//leer string html de la cola
 				pagString = colaStringHtml.take();
+				System.out.println("a parsear url: "+pagString[0]);
+				System.out.println("a parsear html: "+pagString[1]);
 	
 				String url = pagString[0]; //TODO para completar relativos
-				if (!url.endsWith("/")) {
+				if (!url.endsWith("/")) { //TODO no poner / ni analizar links q terminen en ".algo"
 					url = url + "/";
+					System.out.println("url cambiado: "+url);
 				}
 				String htmlString = pagString[1];
 			
@@ -48,8 +51,8 @@ public class HtmlParser implements Runnable {
 				Pattern pattern2 = Pattern.compile("[\"|']([\\w:/\\.?=\\-]*\\.(mp3|ogg))[\"|']");
 				Matcher matcher2 = pattern2.matcher(htmlString);
 				while (matcher2.find()){
-					System.out.println("audio:");
-					System.out.println(matcher2.group(1));
+					//System.out.println("audio:");
+					//System.out.println(matcher2.group(1));
 					//encolar a cola de rec
 					String aEnc = cleanURL(url,matcher2.group(1));
 					colaRecursos.put(aEnc);
@@ -59,8 +62,8 @@ public class HtmlParser implements Runnable {
 				Pattern pattern4 = Pattern.compile("[\"|']([\\w:/\\.?=\\-]*\\.(css|js))[\"|']");
 				Matcher matcher4 = pattern4.matcher(htmlString);
 				while (matcher4.find()){
-					System.out.println("web:");
-					System.out.println(matcher4.group(1));
+					//System.out.println("web:");
+					//System.out.println(matcher4.group(1));
 					//encolar
 					String aEnc = cleanURL(url,matcher4.group(1));
 					colaRecursos.put(aEnc);
@@ -70,12 +73,15 @@ public class HtmlParser implements Runnable {
 				Pattern pattern3 = Pattern.compile("href\\s*=\\s*[\"|']([\\w:/\\.?=\\-]*)[\"|']");
 				Matcher matcher3 = pattern3.matcher(htmlString);
 				while (matcher3.find()){
-					System.out.println("link:");
-					System.out.println(matcher3.group(1));
+					//System.out.println("link:");
+					//System.out.println(matcher3.group(1));
 					//encolar a cola urls
 					String aEnc = cleanURL(url,matcher3.group(1));
 					colaURLsEntrantes.put(aEnc);
 				}
+				
+				System.out.println("-----");
+				Thread.sleep(10000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -87,7 +93,7 @@ public class HtmlParser implements Runnable {
 		Pattern p = Pattern.compile("(http:\\/\\/)[^\\s]*");
 		Matcher m = p.matcher(resto);
 		while (m.find()){
-			System.out.println(m.group(0));
+			System.out.println("la url "+resto+" es absoluta, no agrego");
 			return resto;
 		}
 		return url + resto;
